457a40550a5cc60dd6e25ec4005b65425e7bee88
Minghao Zhu <minghaoz@andrew.cmu.edu>
2026-03-02 21:10:56 -0800

    Add menu page and create profile page

---
+++ pages/createProfile.js
@@ -0,0 +1,90 @@
+import styles from "../styles/General.module.css";
+import profileStyles from "../styles/Profile.module.css";
+import Head from "next/head";
+import React, { useState, useRef, useEffect } from "react";
+import { useRouter } from "next/router";
+import { EditProfile } from "../components/Profile/EditProfile";
+import { Button, Modal, Form } from "react-bootstrap";
+export default function CreateProfile() {
+    let [user, setUser] = useState({});
+    const router = useRouter();
+    const [showAck, setShowAck] = useState(false);
+    const toggleAck = () => setShowAck(!showAck);
+    const [agree, setAgree] = useState(false);
+
+    const checkboxHandler = () => {
+        setAgree(!agree);
+    };
+    if (typeof window !== "undefined") {
+        user = localStorage.getItem("user");
+        if (!user) {
+            user = {
+                name: "",
+                email: "",
+                school: "Sofia University",
+                year: "Master",
+                exp: "",
+                symp: [],
+                cause: [],
+                created: false,
+            };
+            localStorage.setItem("user", JSON.stringify(user));
+        }
+        user = JSON.parse(localStorage.getItem("user"));
+    }
+    useEffect(() => {
+        setShowAck(true);
+    }, []);
+    return (
+        <div>
+            <div className={styles.container}>
+                <Head>
+                    <title>Create Your Profile</title>
+                    <meta name="description" content="create profile page" />
+                    <link rel="icon" href="/favicon.png" />
+                </Head>
+                <main className={styles.main} style={{ height: "100%" }}>
+                    <h1>Create Your Profile</h1>
+                    <Modal show={showAck} onHide={setShowAck} backdrop="static">
+                        <Modal.Header>
+                            <Modal.Title>Terms and Conditions</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <div style={{ color: "black" }}>
+                                Welcome to Relax.EDU! Please complete your profile before using
+                                our application, so we can better guide you to right resources.
+                                Note:Relax.EDU will not sell or disclose any of your information
+                                for any uses.
+                            </div>
+                            <Form>
+                                <br />
+                                <Form.Check
+                                    type="checkbox"
+                                    id="agree"
+                                    label="I Understand"
+                                    onChange={checkboxHandler}
+                                />
+                            </Form>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button
+                                variant="secondary"
+                                onClick={() => {
+                                    router.push("/");
+                                }}
+                            >
+                                Back
+                            </Button>
+                            <Button variant="primary" disabled={!agree} onClick={toggleAck}>
+                                Continue
+                            </Button>
+                        </Modal.Footer>
+                    </Modal>
+                    <div style={{ marginBottom: "1rem", width: "100%" }}>
+                        <EditProfile currUser={user} firstTime={true} />
+                    </div>
+                </main>
+            </div>
+        </div>
+    );
+}
---
+++ pages/menu.js
@@ -0,0 +1,77 @@
+import styles from "../styles/General.module.css";
+import menuStyles from "../styles/Menu.module.css";
+import { Navbar } from "../components/Navbar";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import React, { useState, useRef, useEffect } from "react";
+import * as Icon1 from "react-icons/im";
+import * as Icon2 from "react-icons/fa";
+export default function Menu() {
+    let [user, setUser] = useState({});
+    if (typeof window !== "undefined") {
+        user = JSON.parse(localStorage.getItem("user"));
+    }
+
+    const router = useRouter();
+    const handleEditProfile = (e) => {
+        router.push("/viewProfile");
+    };
+
+    const handleAppointment = (e) => {
+        router.push("/viewAppointmentHistory");
+    };
+
+    const handleFindTherapist = (e) => {
+        router.push("/viewTherapists");
+    };
+
+    const handleMeetAlumni = (e) => {
+        router.push("/alumniCategory");
+    };
+
+    const handleResource = (e) => {
+        router.push("/resourceBoard");
+    };
+    return (
+        <div
+            className={styles.container}
+            style={{ overflowY: "scroll", height: "90vh" }}
+        >
+            <Head>
+                <title>Main menu</title>
+                <meta name="description" content="main menu" />
+                <link rel="icon" href="/favicon.png" />
+            </Head>
+            <main className={menuStyles.main}>
+                <h1>Main Menu</h1>
+
+                <div className={menuStyles.box} onClick={handleEditProfile}>
+                    <h4>
+                        <Icon1.ImProfile></Icon1.ImProfile> View/Edit Profile
+                    </h4>
+                </div>
+                <div className={menuStyles.box} onClick={handleFindTherapist}>
+                    <h4>
+                        <Icon2.FaUserMd></Icon2.FaUserMd> Find a therapist
+                    </h4>
+                </div>
+                <div className={menuStyles.box} onClick={handleMeetAlumni}>
+                    <h4>
+                        <Icon2.FaUserGraduate></Icon2.FaUserGraduate> Meet with Alumni
+                    </h4>
+                </div>
+                <div className={menuStyles.box} onClick={handleResource}>
+                    <h4>
+                        <Icon2.FaFileImage></Icon2.FaFileImage> Resource Sharing Board
+                    </h4>
+                </div>
+                <div className={menuStyles.box} onClick={handleAppointment}>
+                    <h4>
+                        <Icon2.FaHistory></Icon2.FaHistory> Appointment History
+                    </h4>
+                </div>
+                <Navbar />
+            </main>
+        </div>
+    );
+}
